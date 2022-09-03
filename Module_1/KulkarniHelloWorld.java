7bcee5ded2993048f9c56d4bd5a7a5a45002c0e7
SHRADDHAK2017 <skulkarni@my365.bellevue.edu>
2022-09-03 12:32:08 -0500

    Initial commit

---
+++ Module_1/KulkarniHelloWorld.java
@@ -0,0 +1,11 @@
+public class KulkarniHelloWorld {
+
+    
+    public static void myName(String firstName, String lastName) {
+        System.out.println(String.format("My name is %s %s", firstName, lastName));
+    } // end myName
+
+    public static void main(String[] args) {
+        myName("Shraddha", "Kulkarni"); // Call the myName() method with the values "Shraddha" and "Kulkarni."
+    } // end main
+} // end CodeCommentExample0
